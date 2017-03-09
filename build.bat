3fe16a288b606bd15cde351f43c84471109d8de9
Sébastien <sebgeiser@gmail.com>
2017-03-09 19:14:35 +0100

    New Build test

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+call %nuget% restore TranslateMe.sln
+
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" TranslateMe.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+call %nuget% pack "TranslateMe.nuspec" -NoPackageAnalysis -o Build -verbosity detailed -Version %version% -p Configuration="%config%"
+
+PAUSE
