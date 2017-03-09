622bf3bb4c4a97cbd8edf23360d21e63bb509e85
Sébastien <sebgeiser@gmail.com>
2017-03-09 19:54:58 +0100

    Call everywhere

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TranslateMe
+[![translateme MyGet Build Status](https://www.myget.org/BuildSource/Badge/translateme?identifier=3fd13c95-2c77-43dc-9815-b5c527d0679c)](https://www.myget.org/)
 A C# and WPF apps translation library
 
 In developpement
--- build.bat
+++ build.bat
@@ -16,7 +16,7 @@ if "%nuget%" == "" (
 
 call %nuget% restore TranslateMe.sln
 
-"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" TranslateMe.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+call "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" TranslateMe.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 call %nuget% pack "TranslateMe.nuspec" -NoPackageAnalysis -o Build -verbosity detailed -Version %version% -p Configuration="%config%"
 
