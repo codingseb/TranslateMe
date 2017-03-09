7fc8395b56d34c8e1f477379288670711a28142d
Sébastien <sebgeiser@gmail.com>
2017-03-09 17:53:12 +0100

    nupkg generated in root folder

--- Build.bat
+++ Build.bat
@@ -18,7 +18,7 @@ if "%nuget%" == "" (
 
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild" TranslateMe.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-%nuget% pack "TranslateMe.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "TranslateMe.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="%config%"
 REM %nuget% pack TranslateMe.WPF\TranslateMeWPF.csproj -IncludeReferencedProjects -Build -Version %version% -p Configuration="%config%"
 
 PAUSE
