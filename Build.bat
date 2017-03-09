478a72324d22fb2186b0f61bdd05ada282beb0fd
Sébastien <sebgeiser@gmail.com>
2017-03-09 18:05:01 +0100

    Force the build and the nuget in Release

--- Build.bat
+++ Build.bat
@@ -1,8 +1,8 @@
 @echo Off
-set config=%1
-if "%config%" == "" (
-   set config=Release
-)
+REM set config=%1
+REM if "%config%" == "" (
+   REM set config=Release
+REM )
  
 set version=1.0.0
 if not "%PackageVersion%" == "" (
@@ -16,9 +16,9 @@ if "%nuget%" == "" (
 
 %nuget% restore TranslateMe.sln
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild" TranslateMe.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild" TranslateMe.sln /p:Configuration="Release" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-%nuget% pack "TranslateMe.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="%config%"
+%nuget% pack "TranslateMe.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="Release"
 REM %nuget% pack TranslateMe.WPF\TranslateMeWPF.csproj -IncludeReferencedProjects -Build -Version %version% -p Configuration="%config%"
 
 PAUSE
