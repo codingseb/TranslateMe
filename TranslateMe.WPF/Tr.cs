8d2494fc9608e06a4ce7cea962dee95e63598f01
Sébastien Geiser <sgeiser@precitrame.com>
2018-03-19 15:20:00 +0100

    Refactoring and clear

--- TranslateMe.WPF/Tr.cs
+++ TranslateMe.WPF/Tr.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
--- TranslateMe.WPF/TrEnumAsItemSource.cs
+++ TranslateMe.WPF/TrEnumAsItemSource.cs
@@ -1,27 +1,15 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Data;
-using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
 using System.Windows.Markup;
-using System.Xaml;
 
 namespace TranslateMe.WPF
 {
+    /// <summary>
+    /// To use a enum as ItemSource for ComboBox, ListBox... and translate the displayedText.
+    /// Manage the Language Changes
+    /// </summary>
     public class TrEnumAsItemSource : MarkupExtension
     {
         private DependencyObject targetObject;
